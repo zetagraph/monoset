232f3a61f27d81a215f6faeff1d19b995d9cee25
Andrei Zvonkov <andrei@appnovation.com>
2015-06-09 11:48:41 -0400

    JS clean up trough the new gulp js-beatify task

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,12 @@
     attach: function (context, settings) {
 
       // Smooth Scroll to Anchor Links
-
-      $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+          if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var headerHeight = $('.header-main').outerHeight();
             var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
               $('html,body').animate({
                 scrollTop: target.offset().top - headerHeight
@@ -22,9 +21,12 @@
       });
 
       // SVG Injector
-
       // For testing in IE8
-      if (!window.console){ console = {log: function() {}}; };
+      if (!window.console) {
+        console = {
+          log: function () {}
+        };
+      }
 
       // Elements to inject
       var mySVGsToInject = document.querySelectorAll('img.inject-svg');
@@ -32,13 +34,11 @@
       // Options
       var injectorOptions = {
         evalScripts: 'once',
-        each: function (svg) {
-        }
+        each: function (svg) {}
       };
 
       // Trigger the injection
-      SVGInjector(mySVGsToInject, injectorOptions, function (totalSVGsInjected) {
-      });
+      SVGInjector(mySVGsToInject, injectorOptions, function (totalSVGsInjected) {});
 
     }
   };
